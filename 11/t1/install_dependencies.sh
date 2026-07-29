0ea7faa6ab1b51bc4377739861225207cff00ac6
BetteriX <szabozsolt430@gmail.com>
2026-07-29 14:34:11 +0000

    fixed instalation bug

--- 11/t1/install_dependencies.sh
+++ 11/t1/install_dependencies.sh
@@ -2,8 +2,8 @@
 
 REPO_URI=195275646656.dkr.ecr.eu-west-1.amazonaws.com/cmtr-ook9q7ho
 
-sudo yum update -y
-sudo yum install -y docker
+sudo dnf update -y
+sudo dnf install -y docker
 
 sudo systemctl start docker
 sudo systemctl enable docker
