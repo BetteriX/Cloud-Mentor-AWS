3624d7462a9d486deeba690389b4165bea26dd05
BetteriX <szabozsolt430@gmail.com>
2026-07-29 14:56:11 +0000

    finished task & added buildspec into archive

--- 11/t1/archive.sh
+++ 11/t1/archive.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip -r deployment.zip appspec.yml install_dependencies.sh run_app.sh Dockerfile
+zip -r deployment.zip appspec.yml install_dependencies.sh run_app.sh Dockerfile buildspec.yml
