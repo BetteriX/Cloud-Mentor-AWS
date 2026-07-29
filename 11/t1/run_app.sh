7b2f1c1bcfd126c0b3fc89799f02ea970d9b8f94
BetteriX <szabozsolt430@gmail.com>
2026-07-29 14:37:41 +0000

    fixed running the app

--- 11/t1/run_app.sh
+++ 11/t1/run_app.sh
@@ -3,6 +3,7 @@
 sudo systemctl start docker
 
 REPO_URI=195275646656.dkr.ecr.eu-west-1.amazonaws.com/cmtr-ook9q7ho
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin $REPO_URI
 
 docker stop alpine-httpd-container || true
 docker rm alpine-httpd-container || true
